644ddd1bbe5dc0b4dddc589e88bdcda8d05c26a1
Sagar Verma <sagarverma1702@gmail.com>
2023-05-23 12:52:35 +0530

    Multiply Program

---
+++ Multi.java
@@ -0,0 +1,9 @@
+package GitProject;
+
+public class Multi {
+    public static void main(String args[])
+    {
+        int a =10, b=20, c=12;
+        System.out.println("Sum: "+ (a*b*c));
+    }
+}
