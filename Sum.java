2702f5f300d6767345c4a81aa8fabba1a9777f5e
Sagar Verma <sagarverma1702@gmail.com>
2023-05-23 12:54:12 +0530

    Fixed the editing

--- Sum.java
+++ Sum.java
@@ -1,8 +1,10 @@
+package GitProject;
+
 public class Sum
 {
     public static void main(String args[])
     {
         int a =10, b=20, c=12;
-        System.out.println("Sum: "+ (a+b));
+        System.out.println("Sum: "+ (a+b +c));
     }
 }
